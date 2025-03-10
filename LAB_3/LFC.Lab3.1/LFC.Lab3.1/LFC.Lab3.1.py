7d586845864596932e28eb6fbb72784c09ef3f8a
AndraBod <145751125+AndraBod@users.noreply.github.com>
2025-03-10 14:37:25 +0200

    Add files via upload

---
+++ LAB_3/LFC.Lab3.1/LFC.Lab3.1/LFC.Lab3.1.py
@@ -0,0 +1,57 @@
+﻿def automat_finit():
+    q0 = "q0"
+    q1 = "q1"
+    q2 = "q2"
+    q3 = "q3"
+    q4 = "q4"
+    q5 = "q5"
+    stare = q0  
+    comanda_confirmata = False  
+    bautura_selectata = None  
+
+    while True:
+        print("\n    Meniu Băuturi    ")
+        print("C - Cafea")
+        print("T - Ceai")
+        print("A - Cappuccino")
+        print("H - Ciocolată caldă")
+        print("O - Confirmare comandă")
+        print("D - Anulează comanda\n")
+
+        inp = input("Selectează băutura: ").strip().upper()
+
+        if inp == "C":
+            stare = q1
+            bautura_selectata = "Cafeaua"
+        elif inp == "T":
+            stare = q2
+            bautura_selectata = "Ceaiul"
+        elif inp == "A":
+            stare = q3
+            bautura_selectata = "Cappuccino-ul"
+        elif inp == "H":
+            stare = q4
+            bautura_selectata = "Ciocolata caldă"
+        else:
+            print(f"Eroare: Opțiune invalidă ({inp}). Reîncearcă!\n")
+            continue 
+
+        while True:
+            confirmare = input(f"Confirmi comanda pentru {bautura_selectata}? (O - Da, D - Anulează): ").strip().upper()
+            if confirmare == "O":
+                stare = q5
+                comanda_confirmata = True
+                break 
+            elif confirmare == "D":
+                stare = q0
+                comanda_confirmata = False
+                print("Comanda anulată. Alege din nou o băutură!\n")
+                break
+            else:
+                print("Eroare: Introdu doar 'O' pentru confirmare sau 'D' pentru anulare!")
+
+        if comanda_confirmata:
+            return f"{bautura_selectata} dumneavoastră este gata!"
+
+if __name__ == "__main__":
+    print(automat_finit())
---
+++ LAB_3/LFC.Lab3.2/LFC.Lab3.2/LFC.Lab3.2.py
@@ -0,0 +1,53 @@
+﻿def automat_finit_parcare():
+    liber = "Liber"
+    ocupat = "Ocupat"
+    eroare = "Eroare"
+    stare = liber  
+    locuri = [0] * 5
+
+    while True:
+        print("\n    Gestionare Parcare    ")
+        print("P - Parcare Mașină")
+        print("E - Eliberare Loc")
+        print("S - Stare Parcare")
+        print("Q - Ieșire\n")
+
+        inp = input("Selectează opțiunea: ").strip().upper()
+
+        if inp == "S":
+            status = ''.join(['[X]' if loc else '[ ]' for loc in locuri])
+            print(f"Stare parcare: {status}")
+        elif inp == "P":
+            if 0 not in locuri:
+                print("Eroare: Parcarea este plină!")
+                stare = eroare
+            else:
+                loc = locuri.index(0)
+                locuri[loc] = 1
+                stare = ocupat
+                print(f"Mașina a fost parcată la locul {spot + 1}")
+        elif inp == "E":
+            try:
+                loc_nr = int(input("Introdu numărul locului de eliberat: "))
+                if loc_nr < 1 or loc_nr > len(locuri):
+                    print("Eroare: Loc invalid!")
+                    stare = eroare
+                elif locuri[loc_nr - 1] == 0:
+                    print("Eroare: Locul este deja liber!")
+                    stare = eroare
+                else:
+                    locuri[loc_nr - 1] = 0
+                    stare = liber
+                    print(f"Mașina a părăsit locul {loc_nr}")
+            except ValueError:
+                print("Eroare: Introdu un număr valid!")
+                stare = eroare
+        elif inp == "Q":
+            print("Ieșire din sistem.")
+            break
+        else:
+            print("Eroare: Opțiune invalidă!")
+            stare = eroare
+
+if __name__ == "__main__":
+    automat_finit_parcare()
